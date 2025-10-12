016c53da4e5e4aa0acd455610e42ab8806ad1532
Jason Limfueco <julimfueco@ucsd.edu>
2025-10-12 15:47:20 -0700

    Lab 3.2 complete
    3.2 is a lot of JS background updating. this won't directly affect the site until we mess with the html code to use the links we have
    
    the only html change is removing class = "current"

--- global.js
+++ global.js
@@ -1,5 +1,22 @@
+// Lab 3.1
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
+}
+
+// Lab 3.2.1
+let navLinks = $$('nav a');
+console.log(navLinks);
+
+// Lab 3.2.2
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+console.log('Current page URL: ',currentLink)
+
+// Lab 3.2.3
+if (currentLink) {
+  // or if (currentLink !== undefined)
+  currentLink.classList.add('current');
 }
