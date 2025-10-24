c58c287e28d18de442760aa742ab0fe0f0317acf
Jason Limfueco <julimfueco@ucsd.edu>
2025-10-24 00:43:58 -0700

    Lab 4.2.1 COMPLETE
    = lab 4.2.1 complete with a  index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+// Lab 2.1.2
+import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+
+// Lab 2.1.3
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+
+// Lab 2.1.4
+const projectsContainer = document.querySelector('.projects');
+
+// Lab 2.1.5
+renderProjects(latestProjects, projectsContainer, 'h2');
