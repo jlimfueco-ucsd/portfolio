ca606391b6baeb7661d19fa2374492f5b1bbcea2
Jason Limfueco <julimfueco@ucsd.edu>
2025-10-23 23:49:33 -0700

    LAB 4.1.5, 4.1.6 COMPLETE
    we have templated the work for the projects file
    
    the h1 header is dynamic to the project.json file

--- projects/projects.js
+++ projects/projects.js
@@ -7,5 +7,14 @@ const projects = await fetchJSON('../lib/projects.json');
 // Lab 4.1.3.4
 const projectsContainer = document.querySelector('.projects');
 
+// Filter out hidden ones Lab 4.1.6
+const visibleProjects = projects.filter(p => !p.hidden);
+
 // Lab 4.1.3.5
-renderProjects(projects, projectsContainer, 'h2');
+renderProjects(visibleProjects, projectsContainer, 'h2');
+
+// Lab 4.1.6 – Count projects and update the title
+const titleElement = document.querySelector('.projects-title');
+if (titleElement) {
+  titleElement.textContent = `${visibleProjects.length} Projects`;
+}
